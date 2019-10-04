fb925e247cb111c4b77f4c51156f29928fb99ddb
abbas <s.shafie.s@gmail.com>
2019-10-04 09:19:34 -0700

    TaskRepository changed to keep a user.

--- app/src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ app/src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -1,38 +1,69 @@
 package com.example.taskmanager.repository;
 
+import com.example.taskmanager.model.State;
 import com.example.taskmanager.model.Task;
+import com.example.taskmanager.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskRepository {
     private List<Task> tasks = new ArrayList<>();
-    public static final TaskRepository instance = new TaskRepository();
 
-    public static TaskRepository getInstance() {
-        return instance;
+    public static TaskRepository instance;
+    private User mUser;
+
+    public void setUser(User mUser) {
+        this.mUser = mUser;
     }
 
-    private TaskRepository (){}
 
-    public void insertTask(Task task){
+    public static TaskRepository getInstance() {
+        if (instance == null)
+            return new TaskRepository();
+        else
+            return instance;
+    }
+
+    private TaskRepository() {
+    }
+
+    public void insertTask(Task task) {
         tasks.add(task);
     }
 
-    public void deleteTask(Task task){
+    public void deleteTask(Task task) {
         tasks.remove(task);
     }
 
-    public void editTack(Task task){
+    public void editTask(Task task) {
 
-        for(int i =0; i<tasks.size(); i++){
-            if(tasks.get(i).getUuid().equals(task.getUuid())){
-                tasks.set(i,task);
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).getTaskUuid().equals(task.getTaskUuid())) {
+                tasks.set(i, task);
             }
         }
     }
 
+
     public List<Task> getTasks() {
         return tasks;
+    }
+
+    public List<Task> getTasks(State state) {
+        List<Task> tasksOfCurrentUser = new ArrayList<>();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).getUserId().equals(mUser.getUserId()) && tasks.get(i).getState().equals(state))
+                tasksOfCurrentUser.add(tasks.get(i));
+        }
+        return tasksOfCurrentUser;
+    }
+
+    public User getUser() {
+        return mUser;
+    }
+
+    public void deleteAllTasks() {
+        tasks.clear();
     }
 }
